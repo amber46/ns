08cf77f10f80e14a84d17cb36bf9330b2f3844af
Amberson <amber46@gmail.com>
2022-12-23 09:49:23 +0200

    Create secure_ssh.sh

---
+++ secure_ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/bin/sed -i "s/#Port 22/Port 1111/g" /etc/ssh/sshd_config
+/bin/sed -i "s/#Protocol 2/Protocol 2/g" /etc/ssh/sshd_config
+/bin/sed -i "s/#UseDNS yes/UseDNS no/g" /etc/ssh/sshd_config
+
+service sshd restart
