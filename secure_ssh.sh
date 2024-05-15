9fd73230e8aae1e9b65ce75eb553ca4e6722ef56
Amberson <amber46@gmail.com>
2024-05-15 13:48:34 +0300

    Update secure_ssh.sh

--- secure_ssh.sh
+++ secure_ssh.sh
@@ -3,7 +3,7 @@
 /bin/sed -i "s/#Port 22/Port 1111/g" /etc/ssh/sshd_config
 /bin/sed -i "s/#Protocol 2/Protocol 2/g" /etc/ssh/sshd_config
 /bin/sed -i "s/#UseDNS yes/UseDNS no/g" /etc/ssh/sshd_config
-/din/sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes" /etc/ssh/sshd_config
-/din/sed -i "s/PasswordAuthentication no/PasswordAuthentication yes" /etc/ssh/sshd_config
+/bin/sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+/bin/sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 
 service sshd restart
